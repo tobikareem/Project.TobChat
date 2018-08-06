4f6b3011c2b2f6762cfdeb972c1d3af98f0b8e33
Oluwatobi A. Kareem <okareem1@pride.hofstra.edu>
2018-08-06 10:22:45 -0400

    Added Mobie Signin Page and background image

--- Project.TobChat/Project.TobChat.Mobile/Project.TobChat.Mobile/App.xaml.cs
+++ Project.TobChat/Project.TobChat.Mobile/Project.TobChat.Mobile/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace Project.TobChat.Mobile
 {
@@ -13,7 +8,7 @@ namespace Project.TobChat.Mobile
 		{
 			InitializeComponent();
 
-			MainPage = new Project.TobChat.Mobile.MainPage();
+			MainPage = new Pages.SignIn();
 		}
 
 		protected override void OnStart ()
---
+++ Project.TobChat/Project.TobChat.Mobile/Project.TobChat.Mobile/Pages/SignIn.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project.TobChat.Mobile.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SignIn : ContentPage
+	{
+		public SignIn ()
+		{
+			InitializeComponent ();
+
+		    switch (Device.RuntimePlatform)
+		    {
+                case Device.iOS:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+
+                case Device.Android:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+
+                case Device.WinPhone:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+
+                default:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+		    }
+		}
+	}
+}
